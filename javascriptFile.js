6563f3612ac71bcfd74b8cb4f85f6fe016fddd71
Alan Hynes r00145398 <alan.hynes@mycit.ie>
2018-11-28 18:28:24 +0000

    First change to js file

--- javascriptFile.js
+++ javascriptFile.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Hello World!');
+res.end('Hello World! This is tested with Travis!');
 });
 server.listen(8080);
